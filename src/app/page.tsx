1a74769208733ec99dba1e9a99667a78923e7f3b
Максим Захаров <maks@MBP-Maksim.IGD_Rostelecom>
2025-03-31 16:19:18 +0700

    Удален блок 'Процесс' с раздела 'Автономные ИИ-решения' с главной страницы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
-import Blog from "@/components/Blog/Blog"
-import Contact from "@/components/Contact/Contact"
-import FAQ from "@/components/FAQ/FAQ"
-import Features from "@/components/Features/Features"
-import Hero from "@/components/Hero/Hero"
-import Process from "@/components/Process/Process"
-import Projects from "@/components/Projects/Projects"
-import Services from "@/components/Services/Services"
-import Testimonials from "@/components/Testimonials/Testimonials"
+import Blog from "@/components/Blog/Blog";
+import Contact from "@/components/Contact/Contact";
+import FAQ from "@/components/FAQ/FAQ";
+import Features from "@/components/Features/Features";
+import Hero from "@/components/Hero/Hero";
+import Projects from "@/components/Projects/Projects";
+import Services from "@/components/Services/Services";
+import Testimonials from "@/components/Testimonials/Testimonials";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <Process />
       <Services />
       <Features />
       <Projects />
@@ -21,5 +19,5 @@ export default function Home() {
       <FAQ />
       <Contact />
     </main>
-  )
+  );
 }

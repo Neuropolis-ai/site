f0d8c799b7840720d16e279ec83e5f3cbdc24cde
Максим Захаров <maks@MacBook-Pro-Maksim.local>
2025-04-01 18:47:12 +0700

    Удалена страница About, которая не используется

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["framerusercontent.com", "images.unsplash.com"],
     remotePatterns: [
       {
         protocol: "https",
--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-export default function Page(): ReactNode {
-  return (
-    <div className="container mx-auto py-16 px-4">
-      <h1 className="text-3xl font-bold mb-8">О компании Neuropolis.ai</h1>
-    </div>
-  );
-}
